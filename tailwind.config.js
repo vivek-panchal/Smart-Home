dc177e0c8e5d58f84c2d340be327338211afd41d
vivek-panchal <vivek.nsut@gmail.com>
2023-06-08 08:41:39 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
